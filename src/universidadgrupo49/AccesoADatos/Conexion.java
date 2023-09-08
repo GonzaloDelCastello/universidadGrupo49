dd789795cf7de5cf5ee395b50e985b1f1706a631
GonzaloDelCastello <141686474+GonzaloDelCastello@users.noreply.github.com>
2023-09-08 20:52:47 -0300

    Creamos la conexión
    No la probamos. Nos pide un main.

--- src/universidadgrupo49/AccesoADatos/Conexion.java
+++ src/universidadgrupo49/AccesoADatos/Conexion.java
@@ -5,10 +5,39 @@
  */
 package universidadgrupo49.AccesoADatos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class Conexion {
     
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidadgrupo49";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    
+    private static Connection connection;
+    
+    private Conexion(){}
+    
+    public static Connection getConexion(){
+        if(connection == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar el driver."+ex.getMessage());
+                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error de conexión"+ex.getMessage());
+            }
+            
+            }
+        return connection;
+    }
 }
