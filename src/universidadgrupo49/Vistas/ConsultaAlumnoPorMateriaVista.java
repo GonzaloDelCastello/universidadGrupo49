6b477c1d38a19c4837a27b2e7da1527ecf01ca22
AlexAnders2023 <m.alejandro.q@gmail.com>
2023-09-27 19:19:48 -0300

    consulta alumnos por materia
    consulta alumnos por materia completo

--- src/universidadgrupo49/Vistas/ConsultaAlumnoPorMateriaVista.java
+++ src/universidadgrupo49/Vistas/ConsultaAlumnoPorMateriaVista.java
@@ -10,75 +10,97 @@ import universidadgrupo49.AccesoADatos.MateriaData;
 import universidadgrupo49.Entidades.Alumno;
 import universidadgrupo49.Entidades.Inscripcion;
 import universidadgrupo49.Entidades.Materia;
+import javax.swing.table.DefaultTableColumnModel;
 
 /**
  *
  * @author Usuario
  */
 public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
-  
     
+    private DefaultTableModel modelo = new DefaultTableModel() {
+        public boolean isCellEditable(int f, int c) {
+            return false;
+        }
+    };    
     private List<Materia> listaMat;
     private List<Alumno> listaAlu;
+   
     
     private InscripcionData inscrData;
     private MateriaData matData;
     private AlumnoData aluData;
     
-    
-    private DefaultTableModel modelo;
-    
-        
-    
+//    private DefaultTableModel modelo;
+     
     public ConsultaAlumnoPorMateriaVista() {
         initComponents();
-        
+
+    
         aluData = new AlumnoData();
+        matData = new MateriaData();
         boolean estado = false;        // inicializo variable estado-*-
-        listaAlu = aluData.listarAlumnos(estado);
+        listaAlu = aluData.listarAlumnos(true);
+        listaMat = matData.listarMaterias(true);
+        
         modelo = new DefaultTableModel();
         
         inscrData = new InscripcionData();
         matData = new MateriaData();
         
-        cargaInscripcion();
-        armarCabeceraTabla(); 
-      //  cargaAlumnos();
+        cargarMaterias();
+        armarCabeceraTabla();
+        cargarAlumnos();
     }
+
     
     
                   // METODO SELECCIONAR MATERIA         
      
-    private void cargaInscripcion() {
-    MateriaData matData = new MateriaData(); // instancia MateriaData
-    List<Materia> materias = matData.listarMaterias(true); // recupera todos las materias
-    
+    private void cargarMaterias(){
+   
     cboxMateria.removeAllItems(); // limpia comboBox
            
-    for (Materia materia : materias) {
-       cboxMateria.addItem(materia); // agrega cada Materia al comboBox-*-
+    for (Materia materia : listaMat) {
+       cboxMateria.addItem(materia);// agrega cada Materia al comboBox-*-
     }
 }
-    
-    
-                 // ARMAR CABECERA
+     // ARMAR CABECERA
     
      private void armarCabeceraTabla(){
-        ArrayList<Object> filaCabecera = new ArrayList<>();
-        filaCabecera.add("ID");                             //
-        filaCabecera.add("DNI");                         //titulos tabla-*-
-        filaCabecera.add("Apellido");                       //
-        filaCabecera.add("Nombre");
+         ArrayList<Object> filaCabecera = new ArrayList<>();
+         filaCabecera.add("ID");                         //titulos tabla-*-
+         filaCabecera.add("Apellido");                       //
+         filaCabecera.add("Nombre");
+
+         for (Object rec : filaCabecera) {
+             modelo.addColumn(rec);
+         }
+
+         jTablaAlumnos.setModel(modelo); //aca
+    }  
+    private void borrarFilasTablas(){
+        int indice = modelo.getRowCount()-1;
         
+        for(int i = indice;i>0;i--){
+            modelo.removeRow(i);
+        }
+//        while (modelo.getRowCount() > 0) {
+//            int cantidadFilas = modelo.getRowCount();
+//            modelo.removeRow(cantidadFilas - 1);
+//        }
+    } 
+     
+    private void cargarAlumnos(){
+        borrarFilasTablas();
+        Materia selec = (Materia)cboxMateria.getSelectedItem();
+        listaAlu = inscrData.obtenerAlumnosXMateria(selec.getIdMateria());
+        for(Alumno m:listaAlu){
+        modelo.addRow(new Object []{m.getIdAlumno(),m.getApellido(),m.getNombre()});
         
-        for (Object rec : filaCabecera) {
-            modelo.addColumn(rec);
         }
         
-        jTAlumno.setModel(modelo);
-    }  
-     
-     
+    } 
      
      
                  // METODO CARGA TABLA ALUMNOS    
@@ -96,15 +118,15 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
      
              // MATERIA SELECCIONADA
                
-     public String materiaSeleccionada(List<String> listaMaterias, String materiaSeleccionada) {
-    for (String materia : listaMaterias) {
-        if (materia.equals(materiaSeleccionada)) {
-            return materia;
-        }
-    }
-    return null;
-}
-     
+//     public String materiaSeleccionada(List<String> listaMaterias, String materiaSeleccionada) {
+//    for (String materia : listaMaterias) {
+//        if (materia.equals(materiaSeleccionada)) {
+//            return materia;
+//        }
+//    }
+//    return null;
+//}
+//     
      
     
 
@@ -117,9 +139,9 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
         jList1 = new javax.swing.JList<>();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTAlumno = new javax.swing.JTable();
         cboxMateria = new javax.swing.JComboBox<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTablaAlumnos = new javax.swing.JTable();
 
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
@@ -137,8 +159,13 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
         jLabel2.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         jLabel2.setText("Seleccione una materia");
 
-        jTAlumno.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
-        jTAlumno.setModel(new javax.swing.table.DefaultTableModel(
+        cboxMateria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cboxMateriaActionPerformed(evt);
+            }
+        });
+
+        jTablaAlumnos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -146,25 +173,10 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "ID", "DNI", "Apellido", "Nombre"
+                "Title 1", "Title 2", "Title 3", "Title 4"
             }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
-            };
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        jScrollPane2.setViewportView(jTAlumno);
+        ));
+        jScrollPane2.setViewportView(jTablaAlumnos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -174,15 +186,17 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
                 .addGap(73, 73, 73)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(2, 2, 2)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(18, 18, 18)
-                        .addComponent(cboxMateria, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addComponent(jLabel1)
-                            .addGap(15, 15, 15))
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 385, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(82, Short.MAX_VALUE))
+                        .addComponent(cboxMateria, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 64, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -193,13 +207,17 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(cboxMateria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(33, 33, 33)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(48, Short.MAX_VALUE))
+                .addGap(80, 80, 80)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(91, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cboxMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboxMateriaActionPerformed
+        cargarAlumnos();
+    }//GEN-LAST:event_cboxMateriaActionPerformed
 
     
 
@@ -213,6 +231,6 @@ public class ConsultaAlumnoPorMateriaVista extends javax.swing.JInternalFrame {
     private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTAlumno;
+    private javax.swing.JTable jTablaAlumnos;
     // End of variables declaration//GEN-END:variables
 }
